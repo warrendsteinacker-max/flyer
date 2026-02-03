7f28dbc44fe3baf6d1da80fbc446e944c3b0dbdd
Warren <warrendsteinacker@gmail.com>
2026-02-03 14:26:40 -0500

    n

--- flyer/vite.config.js
+++ flyer/vite.config.js
@@ -1,8 +1,25 @@
+// import { defineConfig } from 'vite'
+// import react from '@vitejs/plugin-react'
+
+// // https://vitejs.dev/config/
+// export default defineConfig({
+//   plugins: [react()],
+//   base: '/flyer/', // Replace 'flyer' with your actual repository name
+
+// })
+
+
+
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/flyer/', // Replace 'flyer' with your actual repository name
+  base: '/flyer/', // Your GitHub repo name
+  server: {
+    port: 3000,      // Changes the listening port
+    host: true,      // Allows network access (Tailscale)
+    strictPort: true // Prevents Vite from switching to 3001 if 3000 is busy
+  }
 })
